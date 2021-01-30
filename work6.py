9af0a9fcc99148ab23bd580552077b7c26b93ae6
Surmalumi <76614831+Surmalumi@users.noreply.github.com>
2021-01-30 08:32:24 +0300

    Update work6.py

--- work6.py
+++ work6.py
@@ -5,11 +5,11 @@ import re
 dict = {}
 with open('work6.txt','r', encoding='utf-8') as file:
     new_file = file.readlines()
-    for discipline in new_file: #пробегаем по списку из строк, полученному из readlines
+    for discipline in new_file:
         total_hours = 0
-        hours = re.findall(r'\d+', discipline) #ищем в строке числа и формируем из них список
-        for hour in hours: #пробегаем по сформированному списку из чисел
-            total_hours += float(hour) #добавляем float от числа к сумме
-        dict[discipline.split()[0]] = total_hours #первое слово в строке - название предмета, его добавляем как key, а сумму - как value
+        hours = re.findall(r'\d+', discipline) 
+        for hour in hours: 
+            total_hours += float(hour) 
+        dict[discipline.split()[0]] = total_hours 
     print(dict)
 
